d74e174f0d99550c07490162918fe7ed207c7756
Aaron Faanes <dafrito@gmail.com>
2022-03-05 22:58:49 -0600

    update demo to use inward nodes too

--- src/demo.ts
+++ src/demo.ts
@@ -2,33 +2,39 @@ import Direction, { DirectionNode } from "parsegraph-direction";
 import { Projector, BasicProjector } from "parsegraph-projector";
 import TimingBelt from "parsegraph-timingbelt";
 import Pizza from "./Pizza";
-import BasicPainted, {MIN_BLOCK_WIDTH, MIN_BLOCK_HEIGHT} from "./BasicPainted";
+import BasicPainted, {
+  MIN_BLOCK_WIDTH,
+  MIN_BLOCK_HEIGHT,
+} from "./BasicPainted";
 import RenderArtist from "./RenderArtist";
 import paintNodeLines from "./paintNodeLines";
 import paintNodeBounds from "./paintNodeBounds";
-import Color from 'parsegraph-color';
+import Color from "parsegraph-color";
 
-const s = 100;
+const s = 50;
 
-const artist = new RenderArtist((proj:Projector, node: DirectionNode)=>{
+const artist = new RenderArtist((proj: Projector, node: DirectionNode) => {
   const ctx = proj.overlay();
   ctx.fillStyle = "grey";
-  paintNodeLines(node, 1, (x:number, y:number, w:number, h:number)=>{
+  paintNodeLines(node, 0.5, (x: number, y: number, w: number, h: number) => {
     console.log("Line", x, y, w, h);
-    ctx.fillRect(x-w/2, y-h/2, w, h);
+    ctx.fillRect(x - w / 2, y - h / 2, w, h);
   });
   ctx.fillStyle = Color.random().asRGB();
-  paintNodeBounds(node, (x:number, y:number, w:number, h:number)=>{
+  paintNodeBounds(node, (x: number, y: number, w: number, h: number) => {
     console.log("Bounds", x, y, w, h);
-    ctx.fillRect(x-w/2, y-h/2, w, h);
+    ctx.fillRect(x - w / 2, y - h / 2, w, h);
   });
   return false;
 });
 
-const makeNode = ():DirectionNode=>{
+const makeNode = (): DirectionNode => {
   const node = new DirectionNode();
   const val = new BasicPainted(node, artist);
-  val.setSize(MIN_BLOCK_WIDTH + Math.random() * s, MIN_BLOCK_HEIGHT + Math.random() * s);
+  val.setSize(
+    MIN_BLOCK_WIDTH + Math.random() * s,
+    MIN_BLOCK_HEIGHT + Math.random() * s
+  );
   node.setValue(val);
   return node;
 };
@@ -40,14 +46,15 @@ const buildGraph = () => {
   const dirs = [
     Direction.FORWARD,
     Direction.DOWNWARD,
+    Direction.INWARD,
     Direction.UPWARD,
-    Direction.BACKWARD
+    Direction.BACKWARD,
   ];
-  for (let i = 0; i < 10; ++i) {
+  for (let i = 0; i < 20; ++i) {
     const n = makeNode();
-    let dir:Direction;
-    while (!dir || par.hasNode(dir)) {
-      dir = dirs[Math.floor(Math.random()*dirs.length)];
+    let dir = Direction.NULL;
+    while (dir === Direction.NULL || par.hasNode(dir)) {
+      dir = dirs[Math.floor(Math.random() * dirs.length)];
     }
     par.connectNode(dir, n);
     par.pull(dir);
@@ -72,16 +79,22 @@ document.addEventListener("DOMContentLoaded", () => {
   const belt = new TimingBelt();
   container.appendChild(proj.container());
 
+  setTimeout(()=>{
+    proj.overlay();
+    proj.render();
+    proj.overlay().translate(proj.width() / 2, proj.height() / 2);
+    belt.addRenderable(pizza);
+  }, 0);
+
   const pizza = new Pizza(proj);
   pizza.populate(makeNode());
-  belt.addRenderable(pizza);
 
   const refresh = () => {
     const n = buildGraph();
     pizza.populate(n);
     proj.overlay().resetTransform();
     proj.overlay().clearRect(0, 0, proj.width(), proj.height());
-    proj.overlay().translate(proj.width()/2, proj.height()/2);
+    proj.overlay().translate(proj.width() / 2, proj.height() / 2);
     belt.scheduleUpdate();
     const rand = () => Math.floor(Math.random() * 255);
     document.body.style.backgroundColor = `rgb(${rand()}, ${rand()}, ${rand()})`;
